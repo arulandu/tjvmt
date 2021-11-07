9fec29c7eefbed9148bc09669ea1785d0794a322
Claeb101 <calebarulandu@gmail.com>
2021-11-06 22:48:35 -0400

    alpine fixes. cors issue fix attempt

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+        {
+            source: '/:slug*',
+            headers: [
+                {
+                    key: 'Access-Control-Allow-Origin',
+                    value: '*',
+                },
+                {
+                    key: 'Access-Control-Allow-Headers',
+                    value: 'Origin, X-Requested-With, Content-Type, Accept',
+                },
+            ],
+
+        },
+
+    ]
+  },
 }
