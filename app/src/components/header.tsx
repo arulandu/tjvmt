f420383adaa7a75934db708d8e3f736c23f27912
Claeb101 <calebarulandu@gmail.com>
2022-05-22 23:38:10 -0400

    design fixes

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -7,8 +7,8 @@ const Header = () => {
         <header className='fixed z-50 w-full h-16 sm:h-24 px-4 sm:px-12 lg:px-24'>
         <nav className='h-full flex items-center justify-between'>
             <Link href="#home" passHref>
-                <a className="relative w-16 sm:w-24 aspect-[5/3]">
-                    <Image src="/images/logo.png" layout="fill"/>
+                <a className="relative w-16 sm:w-24 aspect-[5/3] opacity-90 hover:opacity-100 transition-all ease-in-out">
+                    <Image src="/images/logo.png" alt="Logo" layout="fill"/>
                 </a>
             </Link>
             <div className='flex items-center space-between'>
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -7,11 +7,11 @@ import Background from '@/components/Background';
 
 const HeroSection = () => {
   return (
-    <section id="hero" className='h-screen flex items-center justify-center'>
-      <div className='m-4 sm:m-12 md:w-3/4 xl:w-1/2'>
+    <section id="hero" className='h-screen flex items-center justify-center bg-transparent'>
+      <div className='m-4 sm:m-12 md:w-3/4 xl:w-1/2 md:scale-90 hover:scale-100 transition-all duration-300 ease-in-out'>
         <p className='pr-4 text-white font-semibold text-base sm:text-2xl'>Hello, we are...</p>
         <div className='mt-2 relative w-full aspect-[18/5]'>
-          <Image src='/images/nametag.png' layout='fill' objectFit='cover'/>
+          <Image src='/images/nametag.png' alt="TJHSST Varsity Math Team" layout='fill' objectFit='cover'/>
         </div>
         <p className='ml-4 mt-4 text-white font-semibold text-base sm:text-2xl text-right'>...one of the most successful math teams in the nation.</p>
       </div>
@@ -21,9 +21,9 @@ const HeroSection = () => {
 
 const ExperienceCard = ({title, content, img}: {title: string, content: JSX.Element[], img: string}) => {
   return (
-    <div className='w-96 xl:w-[36rem] mx-2 my-12 md:m-12 p-9 flex flex-col items-center border-4 border-white bg-navy bg-opacity-75'>
+    <div className='w-96 xl:w-[36rem] mx-2 my-12 md:m-12 p-9 flex flex-col items-center border-solid border border-white bg-navy bg-opacity-75'>
       <div className='border-solid border-4 border-white relative w-40 sm:w-52 xl:w-64 aspect-[3/4]'>
-        <Image src={img} layout='fill'/>
+        <Image src={img} alt={title} layout='fill'/>
       </div>
       <h3 className='mt-12 self-start text-white font-bold text-2xl xl:text-4xl'>{title}</h3>
       <ul>
@@ -31,12 +31,12 @@ const ExperienceCard = ({title, content, img}: {title: string, content: JSX.Elem
         content.map((item, ind) => {
           const key = title+new String(ind)
           return (
-            <div key={key} className='mt-6 flex justify-start'>
+            <li key={key} className='mt-6 flex justify-start'>
               <div className='mt-0 mb-auto w-12 xl:w-24 relative aspect-square'>
                 <p className='text-pink text-2xl xl:text-4xl'>▹</p>
               </div>
-              <li className='text-white text-base xl:text-2xl'>{item}</li>
-            </div>
+              <p className='text-white text-base xl:text-2xl'>{item}</p>
+            </li>
           );
         })
       }
@@ -76,7 +76,7 @@ const LeadershipSection = ({officers}: {officers: {picture: string, position: st
       {/* <div className='w-full flex items-start'> */}
         <div className='flex-grow'>
           <div className='relative aspect-[13/10]'>
-            <Image src="/images/team.png" layout='fill' className=''/>
+            <Image src="/images/team.png" alt="Team Picture" layout='fill' className=''/>
           </div>
           <h2 className='mt-8 text-white text-5xl font-bold'>Leadership</h2>
           <p className='mt-4 text-white text-2xl font-light'>By giving lectures and organizing activities, our officers work regularly to provide a great VMT experience for everyone, and VMT would not be possible without the help of our sponsors: 
@@ -88,13 +88,13 @@ const LeadershipSection = ({officers}: {officers: {picture: string, position: st
           {
             officers.map((officer) => {
               return (
-                <div className='m-2 group flex justify-center'>
+                <div key={officer.name} className='m-2 group flex justify-center'>
                   <div className='relative w-64 aspect-[3/2] '>
                     <div className='absolute z-20 w-full bg-white opacity-0 -bottom-4 group-hover:bottom-0 group-hover:opacity-50 transition-all ease-in-out duration-200'>
                       <p className='text-navy text-base font-bold text-center'>{officer.name} ({officer.position})</p>
                       {/* <p className=''></p> */}
                     </div>
-                    <Image src={officer.picture} layout='fill' className='' />
+                    <Image src={officer.picture} alt={`${officer.name} (${officer.position})`} layout='fill' className='' />
                   </div>
                 </div>
               );
