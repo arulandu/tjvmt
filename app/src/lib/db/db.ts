6c7362bdaa53d61a2de9fd38fdb23e7c87b4d3a6
Claeb101 <calebarulandu@gmail.com>
2022-09-08 11:12:23 -0400

    wait i figured it out

--- app/src/lib/db/db.ts
+++ app/src/lib/db/db.ts
@@ -1,21 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-// declare global {
-//   var __globalDb: PrismaClient | undefined
-// }
+declare global {
+  var __globalDb: PrismaClient | undefined
+}
 
-// const db = global.__globalDb || new PrismaClient()
-// if(process.env.NODE_ENV !== 'production') global.__globalDb = db
-
-let db: PrismaClient = new PrismaClient()
-
-// if (process.env.NODE_ENV === 'production') {
-//   db = new PrismaClient()
-// } else {
-//   if (!global.prisma) {
-//     global.__globalDb = new PrismaClient()
-//   }
-//   db = global.__globalDb
-// }
+const db = global.__globalDb || new PrismaClient()
+if(process.env.NODE_ENV !== 'production') global.__globalDb = db
 
 export {db};
