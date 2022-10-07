4822d1e70b4d22cea860d8bb3e1fac54ce7ed13c
Claeb101 <calebarulandu@gmail.com>
2022-10-06 22:12:00 -0400

    oauth redirect fix

--- app/src/pages/api/auth/ion/index.ts
+++ app/src/pages/api/auth/ion/index.ts
@@ -1,21 +1,20 @@
+import { parsePath } from './../../../../lib/api/parsePath';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {  
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const path = req.query.path as string;
-  let parts = (new RegExp(/^\w*:\/\/[^\/]*/)).exec(path);
-  const hostname = parts.length == 0 ? '' : parts[0];
-  parts = (new RegExp(/\/[\w|\/]*$/)).exec(path);
-  const route = parts.length == 0 ? '' : parts[0]
+  const { baseUrl } = parsePath(req.query.path as string)
 
   const state = encodeURIComponent(JSON.stringify({
-    origin: route
+    origin: path
   }))
 
-  const redirect_uri = `${hostname}/api/auth/ion/callback`
+  const redirect_uri = `${baseUrl}/api/auth/ion/callback`
   const reqURL = `https://ion.tjhsst.edu/oauth/authorize?response_type=code&client_id=${process.env.ION_CLIENT_ID}&redirect_uri=${redirect_uri}&scope=read&state=${state}`;
-  
+
   return res
-    .setHeader('Access-Control-Allow-Origin', `${process.env.BASE_URL}`)
+    .setHeader('Access-Control-Allow-Origin', `${baseUrl}`)
     .setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
     .setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     .redirect(301, reqURL)
