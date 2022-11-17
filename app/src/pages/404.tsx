34873f0e8a10208f3981a24cc518ca595aa73a2e
Claeb101 <calebarulandu@gmail.com>
2022-11-16 19:57:58 -0500

    404 login info because catloe made me

--- app/src/pages/404.tsx
+++ app/src/pages/404.tsx
@@ -1,13 +1,36 @@
 import { Layout } from '@/components/layout';
+import OutlineButton from '@/components/OutlineButton';
+import { useSession } from '@/components/SessionProvider';
+import { useEffect, useState } from 'react';
 import Error from 'next/error'
+import { useRouter } from 'next/router';
 
 const Custom404 = () => {
+  const { session } = useSession()
+  const router = useRouter()
+  const [path, setPath] = useState(router.asPath)
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setPath(window.location.href.match(/^[\w:\/\d]*\//)[0] + 'dashboard')
+    }
+  }, [path])
+
   return (
     <Layout>
-      <section className='h-screen flex justify-center items-center'>
-        <p className=' text-white text-xl md:text-3xl text-center mx-4'>Hmm... this page doesn't exist. Maybe try to log in or go back?</p>
+      <section className='h-screen flex flex-col justify-center items-center'>
+        {/* <div> */}
+          <p className=' text-white text-xl md:text-3xl text-center mx-4'>Hmm... this page doesn't exist.</p>
+          {session ? null :
+            <div className='mt-4 flex items-center'>
+              <p className='text-white text-lg md:text-xl font-light text-center'>Maybe try logging in...?</p>
+              <a className='' href={`/api/auth/ion?path=${path}`}>
+                <OutlineButton className='mt-4 md:ml-4 md:mt-0' name="Log In" />
+              </a>
+            </div>
+          }
+        {/* </div> */}
+
       </section>
-      {/* <Error statusCode={404} /> */}
     </Layout>
   );
 }
