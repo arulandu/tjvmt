439e7f226cc5f6a1139e179add185586b6d0ac0c
Samarth <samarthmbhargav@gmail.com>
2024-01-19 10:48:05 -0500

    modified hmmt scheme

--- app/src/pages/api/selection/grade.ts
+++ app/src/pages/api/selection/grade.ts
@@ -16,8 +16,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const tsts = await db.tST.findMany({});
     const tstId2Name = Object.fromEntries(tsts.map(t => [t.id, t.name]))
     
+    let subjTsts = ["6531582f50974206aa817aa0", "65348523fd708a86f6224b66", "65399607914b8a550b0fc28a", "653996c2b5d493a82e95163c"];
+
     const indexes = users.map(user => {
       let subs = user.submissions.filter(s => selection.weights[tstId2Name[s.tstId]]).sort((a, b) => b.index-a.index)
+      if (req.body.selectionId == '65aa8dd94041e83ab8038c90'){
+        // HMMT 24 Protocol: Remove least subject TST
+         const subjTstSubmissions = subs.filter(sub => subjTsts.includes(sub.tstId));
+         if (subjTstSubmissions.length > 0) {
+            const minIndexSub = subjTstSubmissions.reduce((minSub, currentSub) => currentSub.index < minSub.index ? currentSub : minSub);
+            subs = subs.filter(sub => sub !== minIndexSub);
+         } 
+      }
       if(subs.length === 0) return 0;
       let subNames = subs.map(s => tstId2Name[s.tstId])
 
